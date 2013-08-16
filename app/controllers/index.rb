17ebc0688e271b90cdc7444d40db046df74c4436
Apprentice <apprentice@dbc15.local>
2013-08-15 18:03:33 -0700

    finish challenge

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,9 +9,17 @@ get '/events/:id/show' do |id|
 end
 
 get '/events/new' do
-  #TODO IMPLEMENT ME
+  erb :new_event
+#TODO IMPLEMENT ME
 end
 
 post '/events/create' do
+  @event = Event.new(params[:event])
+  if @event.save
+    redirect '/'
+  else
+    @errors = @event.errors
+    erb :new_event
+  end
   #TODO IMPLEMENT ME
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,45 @@
 class Event < ActiveRecord::Base
+  require 'date'
+
+  validates :date, :title, :organizer_name, :organizer_email, presence: true # event title is empty
+  validate :valid_date_format # date is a valid format
+  validate :validate_date_in_the_future # date is in the future
+  validates :title, uniqueness: true # event title is already taken
+  validates :organizer_email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i,
+    message: "Invalid Email" }
+  
+
+  def valid_date_format
+    unless self.date && test_for_valid_date
+      errors.add(:date, " must be a valid date format")
+    end
+  end
+
+  def test_for_valid_date
+    if self.date
+      Date.valid_date?(self.date.year, self.date.month, self.date.day)
+    else
+      false
+    end
+  end
+
+  def validate_date_in_the_future
+    current_date = Date.today
+    if self.date && self.date < current_date
+      errors.add(:date, " must be at a future date") 
+    end
+  end
+
 end
+
+
+
+
+# errors.full_messages
+
+# if @record.errors.any?
+#   <ul>
+#     <% @record.errors.each do |error| 
+#       <li>error</li>
+
+# <input type="date">
